49fa4faa6bc378a05e7d2c882aeed9a31851fd0d
GaoYang <GaoYangBenYang@outlook.com>
2026-04-01 10:32:38 +0800

    chore(project): 清理项目缓存和IDE配置文件
    - 移除Cargo创建的缓存目录标签文件CACHEDIR.TAG
    - 删除IntelliJ IDEA工作区配置文件workspace.xml
    - 清理项目中的临时配置和缓存相关文件

--- target/debug/deps/petrokit-681ea21c982a3e13.d
+++
@@ -1,5 +0,0 @@
-C:\Repositories\petrokit\target\debug\deps\petrokit-681ea21c982a3e13.d: src\lib.rs
-
-C:\Repositories\petrokit\target\debug\deps\libpetrokit-681ea21c982a3e13.rmeta: src\lib.rs
-
-src\lib.rs:
--- target/debug/deps/petrokit-c1d5dc5639992b6e.d
+++
@@ -1,5 +0,0 @@
-C:\Repositories\petrokit\target\debug\deps\petrokit-c1d5dc5639992b6e.d: src\lib.rs
-
-C:\Repositories\petrokit\target\debug\deps\libpetrokit-c1d5dc5639992b6e.rmeta: src\lib.rs
-
-src\lib.rs:
